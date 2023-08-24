cb11d2c2cb0570c637e4698e507efa1667e4354c
ThiagoDuart4 <arteslima135@gmail.com>
2023-08-24 14:39:38 -0300

    Layout login, REACT ICON, ENVIO DE USER

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,11 +1,64 @@
 import React from 'react'
 import style from '../login/login.module.css'
+import {Link } from "react-router-dom";
+import { useState } from 'react';
 
-const login = () => {
+import { FcGoogle } from 'react-icons/fc';
+
+const Login = () => {
+
+  const [email,setEmail] = useState('');
+  const [senha,setSenha] = useState('');
+
+
+
+   const handleSubmit = (e) =>{
+    e.preventDefault();
+    const user = {
+      email,
+      senha
+    }
+
+   }
   return (
     <div className={style.login}>
+      <section className={style.image}>
+        Imagem
+      </section>
+      <section className={style.EnterLogin}>
+        <div className={style.EnterLoginInfo}>
+        <h1>Faça o login </h1>
+        <div className={style.FormLogin}>
+        <form  onSubmit={handleSubmit}>
+            <label >
+              Email
+              <input type="email" name='email' onChange={(e)=>{
+              setEmail(e.target.value)
+              }}/>
+            </label>
+            <label >
+              Senha
+              <input type="password" name='senha'   onChange={(e)=>{setSenha(e.target.value)}}/>
+            </label>
+            <hr/>
+            <input type="submit" value="Logar" name='logar' />
+         </form>
+
+          <p>Não tem uma conta?<span><Link to ='/cadastrar'>Crie uma</Link></span></p>
+          <p>Esqueci minha senha</p>
+    <div className={style.SocialBtn}>
+    <button className={style.GoogleBtn}>
+           <FcGoogle  className={style.Gicon}/>
+            <h3>Continue com Google</h3>
+          </button>
+    </div>
+         
+        </div>
+        
+        </div>
+      </section>
     </div>
   )
 }
 
-export default login
+export default Login
